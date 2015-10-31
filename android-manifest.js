4ab7e1e8247ec2f4733a4914267355a8320071b1
Panos Sakkos <panos.sakkos@gmail.com>
2015-10-31 20:14:04 +0100

    Added android web app manifesto

---
+++ android-manifest.js
@@ -0,0 +1,44 @@
+{
+  "name": "One Passwords Web App",
+  "icons": [
+    {
+      "src": "{{site.baseurl}}/img/icon-36p.png",
+      "sizes": "36x36",
+      "type": "image/png",
+      "density": 0.75
+    },
+    {
+      "src": "{{site.baseurl}}/img/icon-48p.png",
+      "sizes": "48x48",
+      "type": "image/png",
+      "density": 1.0
+    },
+    {
+      "src": "{{site.baseurl}}/img/icon-72p.png",
+      "sizes": "72x72",
+      "type": "image/png",
+      "density": 1.5
+    },
+    {
+      "src": "{{site.baseurl}}/img/icon-96p.png",
+      "sizes": "96x96",
+      "type": "image/png",
+      "density": 2.0
+    },
+    {
+      "src": "{{site.baseurl}}/img/icon-144p.png",
+      "sizes": "144x144",
+      "type": "image/png",
+      "density": 3.0
+    },
+    {
+      "src": "{{site.baseurl}}/img/icon-192p.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "density": 4.0
+    }
+  ],
+  "start_url": "index.html",
+  "display": "standalone",
+  "orientation": "landscape"
+}
