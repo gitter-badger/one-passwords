74418c034af6f5b1ce0ac4f2799fbc52338cf6bd
Panos Sakkos <panoss@microsoft.com>
2015-11-01 20:48:10 +0100

    Update password-blockchain.js

--- src/password-blockchain.js
+++ src/password-blockchain.js
@@ -1,25 +1,23 @@
-
-function getPassword(passphrase, serviceName) {
-
+function getBlockchainPassword(passphrase, serviceName) {
   // Highest prime number available in the Bitcoin blockchain to use as the modulo
-  primeBlock = 381323;
+  var primeBlock = 381323;
 
   // No (security) need to make the user remember case sensitive service names
   serviceName = serviceName.toLowerCase();
 
-  // Blockchain's Block which its hash is the target password
-  passwordBlock = parseInt('0x' + sha256(passphrase + serviceName)) % primeBlock;
+  // Block chain's block which its hash is the password
+  var passwordBlock = parseInt('0x' + sha256(passphrase + serviceName)) % primeBlock;
 
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      block = JSON.parse(xhttp.responseText);
+      var block = JSON.parse(xhttp.responseText);
         document.getElementById('password').value = block.data.blockhash;
     }
   }
 
-  url = 'https://chain.so/api/v2/get_blockhash/BTC/' + passwordBlock;
+  var url = 'https://chain.so/api/v2/get_blockhash/BTC/' + passwordBlock;
   xhttp.open('GET', url, true);
   xhttp.send();
 }
