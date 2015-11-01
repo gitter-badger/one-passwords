0dea287b52d2ef2ec1c29dcfac7309519ada8587
Panos Sakkos <panos.sakkos@gmail.com>
2015-11-02 00:28:44 +0100

    updated js

--- js/password-blockchain.js
+++ js/password-blockchain.js
@@ -5,10 +5,10 @@ function getBlockchainPassword(passphrase, serviceName) {
   // No (security) need to make the user remember case sensitive service names
   serviceName = serviceName.toLowerCase();
 
-  // Block chain's block which its hash is the password
-  var passwordBlock = parseInt('0x' + sha256(passphrase + serviceName)) % primeBlock;
+  var block = parseInt('0x' + sha256(passphrase + serviceName)) % primeBlock;
 
   var xhttp = new XMLHttpRequest();
+  var url = 'https://chain.so/api/v2/get_blockhash/BTC/' + block;
 
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -17,7 +17,6 @@ function getBlockchainPassword(passphrase, serviceName) {
     }
   }
 
-  var url = 'https://chain.so/api/v2/get_blockhash/BTC/' + passwordBlock;
   xhttp.open('GET', url, true);
   xhttp.send();
 }
